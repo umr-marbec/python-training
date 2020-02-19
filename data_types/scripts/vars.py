34f0496a5cc5da9ec2615b645061f057f57ac88d
barriern <nicolas.barrier@ird.fr>
2020-02-19 10:09:11 +0100

    Updates in list + some slight changes in running n# Veuillez saisir le message de validation pour vos modifications. Les lignes

--- data_types/scripts/vars.py
+++ data_types/scripts/vars.py
@@ -1,27 +1,13 @@
 x = 1  # int
-x = 'string'  # string
 x = 2.3  # float
+x = 'string'  # string
 type(x)  # class of the object
 dir(x)  # list of methods/attributes
 help(x)  # help about methods
 
+# stores the output of the cap. method 
+# in a new object
+y = x.capitalize()
+
 x = 'string'
 x[0] = 1
-
-x = [0, 1, 2]
-y = x 
-print('x = ', x, 'y = ', y)
-x[1] = 'oups'
-print('x = ', x, 'y = ', y)
-print(id(x), id(y))
-
-x = [0, 1, 2]
-print(id(x))
-x.append(1)
-print(id(x))
-
-x = [0, 1, 2]
-y = x.copy()
-print('x = ', x, 'y = ', y)
-x[1] = 'oups'
-print('x = ', x, 'y = ', y)
--- introduction/scripts/hello.py
+++ introduction/scripts/hello.py
@@ -1,2 +1,4 @@
 import sys
+
+# my first program (comment)
 print('hello ', sys.argv)
