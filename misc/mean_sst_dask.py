899f741ab80e2edeb711cefa8bbabbda9c23eeef
barriern <nicolas.barrier@ird.fr>
2021-08-25 11:56:40 +0200

    Update mean sst computation

--- misc/mean_sst_dask.py
+++ misc/mean_sst_dask.py
@@ -6,12 +6,20 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.11.4
+#       jupytext_version: 1.10.3
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
+
+# # Computation of global mean SST
+#
+# In this example, we illustrate possible bad choices of chunk when computing horizontal mean SST time-series and time-average SST.
+
+# # Extraction of data
+#
+# First, the SST data is extracted, as well as the cell surfaces.
 
 from dask.diagnostics import Profiler, ResourceProfiler, CacheProfiler, visualize
 import xarray as xr
@@ -23,28 +31,37 @@ data = data.isel(olevel=0)
 mesh = xr.open_dataset('data/mesh_mask_eORCA1_v2.2.nc')
 mesh = mesh.isel(z=0, t=0)
 
-volume = mesh['e2t'] * mesh['e3t_0'] * mesh['e1t']
-volume
+surface = mesh['e2t'] * mesh['e1t']
+surface
 
-chunk = {'time_counter': 70}
-#chunk = {'x': 150, 'y':100}
+# ## Bad choice of chunks
+#
+# First, let's make a try by divinding our dataset into tiles:
+
+chunk = {'x': 150, 'y':100}
 thetao = data['thetao'].chunk(chunk)
 thetao
 
-tmean = (thetao * volume).sum(dim=['x', 'y']) / volume.sum(dim=['x', 'y'])
+# The surface array is also decomposed into the same chunks
+
+surfbis = surface.chunk(chunk)
+surfbis
+
+# Now, the mean time-series is computing by weighting using the cell surface:
+
+tmean = (thetao * surfbis).sum(dim=['x', 'y']) / surfbis.sum(dim=['x', 'y'])
 
 # %%time
 with Profiler() as prof, ResourceProfiler(dt=0.25) as rprof, CacheProfiler() as cprof:
     tmean.compute()
 
-visualize([prof, rprof, cprof])
+visualize([prof, rprof, cprof], show=False)
 
 tmean.data.visualize()
 
 l = tmean.plot()
 
-#thetao = thetao.chunk({'time_counter': -1, 'x': 150, 'y': 100})
-thetao
+# Now, the time-average map is computed:
 
 time_mean = thetao.mean(dim='time_counter')
 
@@ -52,7 +69,39 @@ time_mean = thetao.mean(dim='time_counter')
 with Profiler() as prof, ResourceProfiler(dt=0.25) as rprof, CacheProfiler() as cprof:
     time_mean.compute()
 
-visualize([prof, rprof, cprof])
+visualize([prof, rprof, cprof], show=False)
+
+time_mean.data.visualize()
+
+l = time_mean.plot(robust=True, cmap=plt.cm.jet)
+
+# ## Better choice of chunks
+#
+# The performance in the above are disappointing. This this due to a bad chunking choice. If the SST is now chunked along the time only.
+
+chunk = {'time_counter': 70}
+thetao = data['thetao'].chunk(chunk)
+thetao
+
+tmean = (thetao * surface).sum(dim=['x', 'y']) / surface.sum(dim=['x', 'y'])
+
+# %%time
+with Profiler() as prof, ResourceProfiler(dt=0.25) as rprof, CacheProfiler() as cprof:
+    tmean.compute()
+
+visualize([prof, rprof, cprof], show=False)
+
+tmean.data.visualize()
+
+l = tmean.plot()
+
+time_mean = thetao.mean(dim='time_counter')
+
+# %%time
+with Profiler() as prof, ResourceProfiler(dt=0.25) as rprof, CacheProfiler() as cprof:
+    time_mean.compute()
+
+visualize([prof, rprof, cprof], show=False)
 
 time_mean.data.visualize()
 
