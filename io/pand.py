d103dab1487b5efd91f76b1bfd7bbef4925ca25e
barriern <nicolas.barrier@ird.fr>
2021-10-15 12:42:33 +0200

    Update pandas

--- io/pand.py
+++ io/pand.py
@@ -26,28 +26,26 @@ import pandas as pd
 
 data = pd.read_csv('./data/nina34.csv', 
                    delim_whitespace=True,  # use spaces as delimiter
-                   skipfooter=2,   # skips the last 2 lines
+                   skipfooter=3,   # skips the last 2 lines
                    na_values=-99.99,  # sets missing values  
                    engine='python'  # sets engine to Python (default C does not support skip footer)
                    )
-
-print(data)
+data
 # -
 
 # It returns a [pandas.DataFrame](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.html) object.
 
 # To get the names of the line and columns:
 
-print(data.index) # recovers the labels of the columns
-print(data.columns)  # recovers the column indexes
+data.index
+
+data.columns
 
 # To display some lines at the beginning or at the end:
 
-# beginning
-print(data.head(3))
+data.head(3)
 
-# end
-print(data.tail(3))
+data.tail(3)
 
 # ## Data extraction
 #
@@ -59,50 +57,46 @@ print(data.tail(3))
 #
 # ### Extracting one column
 #
-# To extract a whole column:
+# To extract a whole column, we can provide a list of column names as follows:
 
-col = data['JAN']
-print(col)
+col = data[['JAN', 'FEB']]
+col
 
 # ### Using names
 #
-# Extracting data using column and row names is done by using the [loc] https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.loc.html#pandas-dataframe-loc method.
+# Extracting data using column and row names is done by using the [loc](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.loc.html#pandas-dataframe-loc) method.
 
-# Extracting one column
-dataex = data.loc[:, 'JAN']
-print(dataex)
+dataex = data.loc[:, ['JAN', 'FEB']]
+dataex
 
-# Extracting one row
-dataex = data.loc[1950, :]
-print(dataex)
+dataex = data.loc[[1950, 1960], :]
+dataex
 
-# Combining both
-dataex = data.loc[slice(1950, 1953), ['JAN', 'FEB']]
-print(dataex)
+dataex = data.loc[1950:1953, ['JAN', 'FEB']]
+dataex
 
 # ### Using indexes
 #
 # Extracting data using column and row names is done by using the [iloc](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.iloc.html#pandas-dataframe-iloc) method.
 
-# Extracting one column
-dataex = data.iloc[:, 0]
-print(dataex)
+dataex = data.iloc[:5, 0]
+dataex
 
-# Extracting one row
 dataex = data.iloc[2, :]
-print(dataex)
+dataex
 
-# Combining both
 dataex = data.iloc[slice(2, 6), [0, 1]]
-print(dataex)
+dataex
+
+dataex = data.iloc[slice(2, 6), :].loc[:, ['OCT', 'NOV']]
+dataex
 
 # ## Extracting data arrays
 #
 # To extract the data arrays, use the `values` attributes.
 
-# extract_data into arrays
-array = data.values  # extracts data, no more metadata (column names, etc)
-print(type(array))
+array.shape
+array
 
 # ## Plotting
 #
@@ -111,13 +105,12 @@ print(type(array))
 # +
 import matplotlib.pyplot as plt
 
-data.loc[:, ['JAN', 'FEB']].plot()
-plt.show()
+l = data.loc[:, ['JAN', 'FEB']].plot()
 # -
 
-fig = plt.figure()
-data.loc[1950, :].plot()
-plt.show()
+l = data.loc[1970, :].plot()
+
+l = data.T.loc[:, 1995:2000].plot()
 
 # ## Creating dataframes
 #
@@ -137,69 +130,37 @@ data = pd.DataFrame({'xvalue': x,
                      'yvalue': y,
                      'cat': cat},
                      index=date)
-print(data)
-# -
-
-# To convert a pandas data time series into a [caterogical](https://pandas.pydata.org/pandas-docs/stable/user_guide/categorical.html) data, use the `.astype("category")` method.
-
-# +
-# set into a category (equivalent to R factor)
-print('cat')
-print(data['cat'])
-
-data['cat_c'] = data['cat'].astype("category")
-
-print('cat_c')
-print(data['cat_c'])
-print(data['cat_c'].cat.categories.values)  # extract list of categories
+data
 # -
 
 # ## Mathematical operations
 #
-# Mathematical operations can be done by using the available pandas methods. Note that it is done only on numerical types.
+# Mathematical operations can be done by using the available pandas methods. Note that it is done only on numerical types. By default, the mean over all the rows is performed:
 
-# Computes the mean over the entire data frame
-datam = data.mean()
-print(datam['xvalue'])
+datam = data.loc[:, ['xvalue', 'yvalue']].mean()
+datam
+
+# But you can also compute means over columns:
 
 # mean over the second dimension (columns)
-datam = data.mean(axis=1)
-print(datam)
+datam = data.loc[:, ['xvalue', 'yvalue']].mean(axis=1)
+datam
 
-# There is also the possibility to do some treatments depending on the value of a caterogical variable.
+# There is also the possibility to do some treatments depending on the value of a caterogical variable (here, the column called `cat`).
 
-datas = data.sort_values(by="cat")
-print(datas)
+data_sorted = data.sort_values(by="cat")
+data_sorted
 
-datas =  data.groupby("cat").size()
-print(datas)
+# You can count the occurrences:
 
-datas =  data.groupby("cat").size()
-print(datas)
+data.groupby("cat").size()
 
-datas =  data.groupby("cat").mean()
-print(datas)
+data.groupby("cat").mean()
 
-datas =  data.groupby("cat").std()
-print(datas)
+data.groupby("cat").std()
 
 # ## Writting a CSV
 #
 # Writting a CSV file is done by calling the [DataFrame.to_csv](https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_csv.html) method.
-
-# +
-import numpy as np
-
-# init a date object: 10 elements with a 1h interval
-date = pd.date_range('1/1/2012', periods=10, freq='H')
-
-x = np.arange(10)
-y = np.arange(10)*0.5
-cat = ['A']*2 + ['C'] + ['A'] + 3*['B'] + ['C'] + ['D'] + ['A']
-
-data = pd.DataFrame({'xvalue': x,
-                     'yvalue': y,
-                     'cat': cat},
-                     index=date)
 
 data.to_csv('data/example.csv', sep=';')
