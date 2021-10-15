63fa67f68b7c2218d9de01a4055ae4c244a4c788
barriern <nicolas.barrier@ird.fr>
2021-10-15 09:33:03 +0200

    Update functions

--- blocks/functions.py
+++ blocks/functions.py
@@ -17,43 +17,42 @@
 #
 # ## Definitions
 #
-# The creation of functions is achieved with the ```def``` keyword. Output values are specified using the `return` function.
+# The creation of functions is achieved with the ```def``` keyword.
+#
+# Compulsory arguments are put at the beginning of the function definition. The order matters.
+#
+# Optional arguments are put at the end of the function definition, along with their default values (defined by an `=` statement). The order does not matter here.
+#
+# Output values are specified using the `return` function. Several variables can be returned (will be returned as `tuple`).
 
 # +
 import numpy as np
 
-def function(arg1, arg2, arg3=0, arg4=-1):
-    # arg1, arg2: compulsory arguments
-    # arg3, arg4: additional arguments with default values
-    # !!! Additional arguments must be after compulsory ones
+def myfunc(arg1, arg2, arg3=0, arg4=-1):
     print('args = ', arg1, arg2, arg3, arg4)
     output = arg1 + arg2 + arg3 * (arg1 + arg2) \
             + arg4*(arg1 - arg2)
     
-    return output, output**2  # returs a 2 elemt. tuple
-
-
-# +
-x1, x2 = function(1, 2) 
-print(x1, x2)
-
-x1, x2 = function(1, 2, arg4=1) 
-print(x1, x2)
-
-x1, x2 = function(1, 2, arg3=1)
-print(x1, x2)
-
-x1, x2 = function(1, 2, arg3=1, arg4=1)
-print(x1, x2)
-
-x = function(1, 2, arg3=1, arg4=1)  # output returned as a tuple
-print(x)
-x1 = x[0]
-x2 = x[1]
-print(x1, x2)
+    return output, np.power(output, 2)  # returs a 2 elemt. tuple
 
 
 # -
+
+x1, x2 = myfunc(1, 2) 
+x1, x2
+
+x1, x2 = myfunc(1, 2, arg4=1) 
+x1, x2
+
+x1, x2 = myfunc(1, 2, arg3=1)
+x1, x2
+
+x1, x2 = myfunc(1, 2, arg3=1, arg4=1)
+x1, x2
+
+x = myfunc(1, 2, arg3=1, arg4=1)  # output returned as a tuple
+x
+
 
 # ## Arguments as reference or values
 #
@@ -67,11 +66,8 @@ print(x1, x2)
 
 # For instance, let's define a function that adds a `y` variable to a `x` variable:
 
-# demonstration of arguments as references/values
 def update(x, y):
-    print('id before ', id(x))
     x += y
-    print('id after.', id(x))
 
 
 # Let's apply the method on an immutable type, for instance an `int`:
@@ -80,23 +76,25 @@ x = 1
 update(x, 10)
 print(x)
 
-# In this case, the `x` value is the same as before. Beside, the `id` values are different. This is because for imutable types, the `+=` statement ultimately creates a new instance (in this case, it is equivalent to `x = x + y`). Same thing holds for a `string` argument:
+# In this case, the `x` value is the same as before. This is because for imutable types, the `+=` statement ultimately creates a new instance (in this case, it is equivalent to `x = x + y`). Same thing holds for a `string` argument:
 
 x = 'string arg'
 update(x, ' final string')
 print(x)
 
-# Now, let's try to use the function on a mutable type, a `list` or `array` for instance:
+# Now, let's try to use the function on a mutable type. If we convert the above strings into lists:
 
-x = [1]
-update(x, [2])
+x = list('string arg')
+update(x, list(' final string'))
 print(x)
+
+# In this example, after the function call, the value of `x` is changed. Same holds for arrays:
 
 x = np.array([1, 2, 3])
 update(x, 2)   # x has been updated in the function
 print(x)
 
-# In the last examples, the reference has been changed. Therefore, outside of the function, the object provided as an argument has been modified.
+# **Therefore, when modifying a variable argument inside a function, be carefull.**
 
 # ## Scope of variables
 #
@@ -105,26 +103,25 @@ print(x)
 
 # +
 x = np.arange(5)
-print(id(x), x)
+print('x before ', x)
 
 def function(x):
-    print(id(x), x)
+    print('x begin func ', x)
 
     x += 5
-    print(id(x), x)
     
     z = x + 10
-    print('func.', 'x', x, 'z', z)
+    print('x end func', x, 'z', z)
 
 function(np.arange(5, 10))
 
-print(id(x), x)
-# print(z)  # causes an error: z undefined
+print('x after ', x)
+#print(z)  # causes an error: z undefined
 # -
 
-# In the above example, the first print in the function shows the values of the array provided in the function call (`np.arange(5, 10)`). The `x` variable defined above cannot be accessed anymore because overwritten by the local one. When the function if finished, the local `x` is destroyed, and the firstly defined `x` is shown by the last print. `z` is only accessible from inside the function.
+# In the above example, the first print in the function shows the values of the array provided in the function call (`np.arange(5, 10)`). The `x` variable defined above cannot be accessed anymore because overwritten by the local one. When the function ends, the local `x` is destroyed, and the firstly defined `x` is shown by the last print. `z` is only accessible from inside the function.
 
-# *Global* variables, defined outside of a function, can be accessed from inside a function.
+# *Global* variables, defined outside of a function, can be accessed from inside a function if they are note overwritten by local ones. **But they are accessed in read-only mode only!**
 
 # +
 # by default, global variable
@@ -136,10 +133,7 @@ def function2():
 function2()
 # -
 
-# <div class="alert alert-danger">
-#   <strong>Warning!</strong> 
-#     By default, a variable <strong>that is assigned</strong> within a function is considered as <i>local</i>. Assignation will therefore fail using a global variable, except if it is declared as <i>global</i> in the function.
-# </div>
+# To overwrite the value of a global variable from inside a function, it must be declared as `global`:
 #
 
 # +
@@ -148,15 +142,13 @@ x = 10
 
 def function3():
 
-    # to assign new values to the global variables,
-    # they need to be declared as 'global'
     global z
     
     z = z + 10     
-    # x = x + 5 # will crash because x not declared global
+    #x = x + 5 # will crash because x not declared global
 
 function3()
-print(z, x)  # z has been updated
+z, x
 
 
 # -
@@ -165,12 +157,9 @@ print(z, x)  # z has been updated
 
 # ### Functions: the ```*args``` argument
 #
-# When the number of arguments is variable, you can use the ```*args``` argument, can contains as many arguments as you want.
+# When the number of arguments is variable, you can use the ```*args``` argument in the function definition. It allows to define any number of additional arguments, which will be provided as a list.
 
 def function2(x, y, *args):
-    # if args are provided
-    # print all the arguments
-    # return a tuple
     print('x = ', x)
     print('y = ', y)
     print('args = ', args)
@@ -178,19 +167,25 @@ def function2(x, y, *args):
 
 function2(3, 'toto')
 
+# Here, no additional argument is provided. Therefore, an empty list is stored in the `args` variable. 
+
 function2(3, 'toto', 5.4)
+
+# Here, one additional argument has been provided. `args` is therefore a list containing one `float` element.
 
 function2(3, 'toto', 5.4, 'z', [0, 3, 4])
 
+# Here, 3 additional arguments are provided. Note that the additional arguments can be of any type, as done in the above.
+
 # ### The ```**kwargs``` argument
 #
-# The `**kwargs` arguments allow to provide additional keyword arguments. 
+# The `**kwargs` arguments allow to provide additional keyword arguments, provided as a `dict`, contrary to `args`, which returns arguments as a `list`. In this case, the arguments are provided using the `key = value` syntax.
 #
 # Imagine you want to define a function that normalizes a time-series, i.e. removes the mean and divides by the standard-deviation:
 #
 # $Y = \frac{X - \overline{X}}{\sigma_X}$
 #
-# Your function should be able to consider include all the possible arguments of the ```numpy.mean``` function. You can either copy/paste
+# Your function should be able to include all the possible arguments of the ```numpy.mean``` function. You can either copy/paste
 # the full list of the ```numpy.mean``` arguments. However:
 #
 # - This is time-consuming
@@ -207,13 +202,23 @@ def stand(x, **kwargs):
     return (x - m) / s
 
 
-# In your function, you allow the possibility to include to your function call a list of keyword arguments. These keyword arguments are used in the `np.mean` function. Therefore, all the keyword arguments of the `numpy.mean` function can be used in your `stand` function:
+# In your function, you allow the possibility to include to your function call a list of keyword arguments. These keyword arguments are used in the `np.mean` function. Therefore, all the keyword arguments of the `numpy.mean` function can be used in your `stand` function. Let's give it a try by creating a dummy variable.
 
 x = np.random.normal(loc=0.0, scale=1.0, size=(1000, 100))
+x.shape
+
+# Now, we call the `stand` function on this array:
 
 out = stand(x)
+out.shape
 
-out = stand(x, axis=0)  # mean computed over the dim 0
+# Here, no keyword arguments has been provided, so the call to the `np.mean` function computes the mean over the entire table, hence returning a numerics. `kwargs` is an empty `dict` object.
+#
+# Now, if we want to specify the axis along which the mean must be computed:
+
+out = stand(x, axis=0)
+
+# In this case, `kwargs` is a dictionary containing one `key/value` couple. In this case, the `mean` returns an array of size $100$. We can use other arguments that are available on the `np.mean` function:
 
 out = stand(x, keepdims=True, axis=0)  # keeping the dimensions
 
@@ -221,12 +226,11 @@ out = stand(x, keepdims=True, axis=0)  # keeping the dimensions
 # However, if you provide a keyword argument that does not exist in the `numpy.mean`, the code will crash.
 
 # +
-# out = stand(x, ddof=1)  # crashes since ddof is no numpy.mean argument
+#out = stand(x, ddof=1)  # crashes since ddof is no numpy.mean argument
 # -
 
-# It would be even better if all the arguments of the `np.std` function could be used as well. It is possible by using dictionaries. 
+# It would be even better if all the arguments of the `np.std` function could be used as well. It is possible by using dictionaries as arguments (instead of `**kwargs`), as done as follows:
 
-# +
 def stand(x, argsmean={}, argsstd={}):
     m = np.mean(x, **argsmean)
     s = np.std(x, **argsstd)
@@ -234,12 +238,20 @@ def stand(x, argsmean={}, argsstd={}):
     print('std' , s.shape)
     return (x - m) / s
 
+
+# In this case, we add two optional dictionary arguments (empty by default), one dictionary containing the arguments that will be used in `np.mean` function (`argsmean`), and one dictionary containing the arguments that will be used in `np.std` function (`argsstd`).
+
+# Now we can create the dictionary arguments as follows:
+
+# +
 # extra arguments for the plot function
 args_mean = {'keepdims':True, 'axis':0}
 
 # extra arguments for the savefig function
 args_std = {'keepdims':True, 'axis':0, 'ddof':1}
 # -
+
+# Now we can see what happens when we call the new `stand` function, with or without the dict arguments:
 
 out = stand(x)
 
@@ -248,18 +260,6 @@ out = stand(x, argsmean=args_mean)  # mean computed over 1st dimensions
 out = stand(x, argsstd=args_std)  # std computed over 1st dimension, removes one dof
 
 out = stand(x, argsmean=args_mean, argsstd=args_std)
-
-# ## Functions: defining input and output types
-#
-# To manage input and output types, see https://docs.python.org/3/library/typing.html. This is done by using `:` after the argument name. 
-#
-# **The function will work without any problems, but the linters (VSCode or other) will show that there is a typing issue.**
-
-import numpy as np
-def function(x : np.ndarray) -> list:
-    output = 2 * x
-    return output
-
 
 # ## Lambda functions
 #
