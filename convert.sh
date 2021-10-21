979e2990c77a0e22196329469b425ec995ed7ef4
barriern <nicolas.barrier@ird.fr>
2021-10-21 17:06:57 +0200

    Update convert file

--- convert.sh
+++ convert.sh
@@ -19,3 +19,28 @@ do
     cd ..
 done
 
+cd misc
+for f in nemo.py eof_analysis.py interpol.py practical_session.py shapefiles.py
+do
+
+    echo $f
+
+    # Convert .py to ipynb
+    jupytext --to notebook $f
+    fout=`echo $f | sed "s/.py/.ipynb/"`
+
+    # execute notebooks
+    jupyter nbconvert --execute --to notebook $fout
+
+    final_file=`echo $fout | sed "s/ipynb/nbconvert.ipynb/"`
+    mv $final_file $fout
+done
+
+for f in dask_covariance.py dask_examples.py mean_sst_dask.py
+do
+    echo $f
+    # Convert .py to ipynb
+    jupytext --to notebook $f
+done
+
+cd ..
